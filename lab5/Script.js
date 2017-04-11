a3a53dc0298ffa2a8bd6020629992dd044befd68
Unknown <nathaporn.w@ku.th>
2017-04-11 21:10:52 +0700

    add link to lab5

---
+++ lab5/Script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('h4').css("text-align", "center");
+    $('div').css("text-align", "center");
+    $('body').css("text-align", "center");
+    $('div').css("background-color", "#c5e3ff");
+    $('body').css("background-color", "#fff5cd");
+    $('divt').css("padding", "5px 10px 20px 10px");
+    $('div').css("width", "200px");
+    $('div').css("margin","auto");
+
+})
